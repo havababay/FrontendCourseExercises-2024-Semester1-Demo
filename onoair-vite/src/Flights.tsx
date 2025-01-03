fa96d2e0350e12a5c1b5f5b3c4e3d2edaa1dcf22
havababay <hava.babay@gmail.com>
2025-01-03 08:19:47 +0200

    connect to firebase flights

--- onoair-vite/src/Flights.tsx
+++ onoair-vite/src/Flights.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
+import { listFlights } from "./firebase/flights/flights";
+import { Flight } from "./model/Flight";
+
 export default function Flights() {
-    return (
-        <div>
-            <h1>Flights</h1>
-            <h1>Flights</h1>
-        </div>
-    );
+  const [flights, setFlights] = useState<Flight[]>([]);
+  useEffect(() => {
+    listFlights().then((flights) => {
+      setFlights(flights);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Flights</h1>
+      <div>{(flights.length > 0) ? flights.map((flight) => 
+        <p key={flight.flightNumber}>{flight.flightNumber}</p>) : "No flights"}</div>
+    </div>
+  );
 }
